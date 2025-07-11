49597e3e3ccba44a627cbca8475a5e7703c09d36
a-jb97 <a_jb97@naver.com>
2025-07-11 21:42:56 +0900

    TravelTableView UI 완성

---
+++ SeSAC_TRAVEL/TravelTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TravelTableViewCell.swift
+//  SeSAC_TRAVEL
+//
+//  Created by 전민돌 on 7/11/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        photoImageView.clipsToBounds = true
+        photoImageView.contentMode = .scaleAspectFill
+        photoImageView.layer.cornerRadius = 15
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 25)
+        titleLabel.numberOfLines = 3
+        subtitleLabel.font = UIFont.systemFont(ofSize: 17)
+        subtitleLabel.textColor = .systemGray2
+        dateLabel.font = UIFont.systemFont(ofSize: 15)
+        dateLabel.textAlignment = .right
+        dateLabel.textColor = .systemGray2
+    }
+}
--- SeSAC_TRAVEL/TravelTableViewController.swift
+++ SeSAC_TRAVEL/TravelTableViewController.swift
@@ -6,21 +6,39 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelTableViewController: UITableViewController {
-
+    
+    let magazineInfo: MagazineInfo = MagazineInfo()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return magazineInfo.magazine.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "travelCell", for: indexPath) as! TravelTableViewCell
+        
+        let dateformat = DateFormatter()
+        dateformat.dateFormat = "yy년 MM월 dd일"
+        let date = dateformat.date(from: magazineInfo.magazine[indexPath.row].date)
+        
+        cell.photoImageView.kf.setImage(with: URL(string: magazineInfo.magazine[indexPath.row].photo_image))
+        cell.titleLabel.text = magazineInfo.magazine[indexPath.row].title
+        cell.subtitleLabel.text = magazineInfo.magazine[indexPath.row].subtitle
+        cell.dateLabel.text = dateformat.string(from: date ?? Date())
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 510
     }
 }
