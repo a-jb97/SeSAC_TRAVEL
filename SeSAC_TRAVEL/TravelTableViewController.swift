e7254ed1054ad5f8236925a2925b5b7fc9f72422
a-jb97 <a_jb97@naver.com>
2025-07-11 15:16:51 +0900

    프로젝트 세팅, MainViewController 삭제, TravelTableViewConstroller 생성

---
+++ SeSAC_TRAVEL/TravelTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TravelTableViewController.swift
+//  SeSAC_TRAVEL
+//
+//  Created by 전민돌 on 7/11/25.
+//
+
+import UIKit
+
+class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+}
