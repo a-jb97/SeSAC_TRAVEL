371a83ab5cd95c253305d587f09c6da5668f177b
a-jb97 <a_jb97@naver.com>
2025-07-12 16:02:40 +0900

    GameViewController 생성 및 오토레이아웃 설정

---
+++ SeSAC_TRAVEL/GameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewController.swift
+//  SeSAC_TRAVEL
+//
+//  Created by 전민돌 on 7/12/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
