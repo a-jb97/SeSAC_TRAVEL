70d1503a702a9cc9c884b9ad500eb9506f1e5937
a-jb97 <a_jb97@naver.com>
2025-07-12 20:49:53 +0900

    TravelInfoTableViewCell 생성 및 오토레이아웃 설정

---
+++ SeSAC_TRAVEL/TravelInfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelInfoTableViewCell.swift
+//  SeSAC_TRAVEL
+//
+//  Created by 전민돌 on 7/12/25.
+//
+
+import UIKit
+
+class TravelInfoTableViewCell: UITableViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var rateSaveLabel: UILabel!
+    @IBOutlet var travelImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+}
---
+++ SeSAC_TRAVEL/TravelInfoTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TravelInfoTableViewController.swift
+//  SeSAC_TRAVEL
+//
+//  Created by 전민돌 on 7/12/25.
+//
+
+import UIKit
+import Kingfisher
+
+class TravelInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+}
