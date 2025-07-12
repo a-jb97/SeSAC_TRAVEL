d4b0ee22b986ab9b02c2336c0e6528bb226b486c
a-jb97 <a_jb97@naver.com>
2025-07-12 19:29:17 +0900

    3.6.9게임 로직 구현(3번까지), 도시 정보 탭 TableViewController 생성

--- SeSAC_TRAVEL/GameViewController.swift
+++ SeSAC_TRAVEL/GameViewController.swift
@@ -8,11 +8,73 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    
+    @IBOutlet var maxNumberTextField: UITextField!
+    @IBOutlet var tappableView: UIView!
+    @IBOutlet var gameTextView: UITextView!
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var playButton: UIButton!
+    
+    var stringNumberList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        uiSetting()
+    }
+    
+    func uiSetting() {
+        maxNumberTextField.textColor = .accent
+        maxNumberTextField.layer.borderWidth = 1
+        maxNumberTextField.layer.borderColor = UIColor.accent.cgColor
+        maxNumberTextField.attributedPlaceholder = NSAttributedString(string: "최대 숫자를 입력해주세요", attributes: [NSAttributedString.Key.foregroundColor: UIColor.systemGray2])
+        
+        gameTextView.text = ""
+        gameTextView.isEditable = false
+        gameTextView.isScrollEnabled = true
+        gameTextView.textColor = .accent
+        
+        resultLabel.text = ""
         
     }
 
+    @IBAction func maxNumberTextFieldDidEndOnExit(_ sender: UITextField) {
+        
+    }
+    
+    @IBAction func playButtonTapped(_ sender: UIButton) {
+        stringNumberList.removeAll()
+        
+        let clapNumbers: [String] = ["3", "6", "9"]
+        var claplist: [String] = []
+        
+        if Int(maxNumberTextField.text ?? "") != nil {
+            for number in 1...Int(maxNumberTextField.text!)! {
+                stringNumberList.append(String(number))
+            }
+            
+            for i in 0..<stringNumberList.count {
+                for j in 0..<clapNumbers.count {
+                    if stringNumberList[i].contains(clapNumbers[j]) {
+                        stringNumberList[i] = "👏"
+                        claplist.append(stringNumberList[i])
+                    }
+                }
+            }
+            
+            gameTextView.text = stringNumberList.joined(separator: ", ")
+            
+            
+            resultLabel.text = "숫자 \(maxNumberTextField.text!)까지 총 박수는 \(claplist.count)번 입니다."
+            
+        } else {
+            maxNumberTextField.text = nil
+            maxNumberTextField.attributedPlaceholder = NSAttributedString(string: "숫자만 입력해주세요!", attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
+        }
+    }
+    
+    @IBAction func viewDidTapped(_ sender: UITapGestureRecognizer) {
+        tappableView.endEditing(true)
+    }
+    
 }
